d63f08919118a8f3023cb1f34e4ef74f7608828f
Ashwin Pandey <ap.ashwinpandey@outlook.com>
2023-10-03 22:35:32 -0400

    question 7

--- Assignment 1/assignment1.sql
+++ Assignment 1/assignment1.sql
@@ -20,6 +20,40 @@ WHERE
     EXTRACT(MONTH from hire_date) IN (5, 11)
 ORDER BY hire_date DESC;
 
+/*
+7.	Display alphabetically the full name, job, salary (formatted as a currency amount incl. thousand separator, but no decimals) and department number for each employee who earns less than the best paid unionized employee (i.e. not the president nor any manager nor any VP), and who work in either SALES or MARKETING department.  
+•	Full name should be displayed as Firstname Lastname and should have the heading Employee. 
+•	Salary should be left-padded with the = symbol till the width of 15 characters. It should have an alias Salary.
+•	You should display ONE row per output line by limiting the width of the 	Employee to 24 characters.
+•	The output lines should look like this sample line (4 columns):
+Jonathon Taylor	SA_REP	=======  $8,600	80
+
+*/
+
+--7
+SELECT
+    SUBSTR(first_name || ' ' || last_name, 1, 24) as Employee,
+    job_id,
+    RPAD('=', 15, '=') || TO_CHAR(salary, '$999,999') as salary,
+    department_id
+FROM (
+    SELECT *
+    FROM employees
+    WHERE department_id IN (20, 80)
+)
+WHERE salary < (
+    SELECT MAX(salary)
+    FROM (
+        select *
+        FROM 
+            employees
+        WHERE employee_id NOT IN (
+            SELECT DISTINCT(NVL(manager_id, 0))
+            FROM employees
+        )
+    )
+)
+ORDER BY Employee, job_id, salary;
 
 
 /*Display department name, city and number of different jobs in each department. If city is null, you should print Not Assigned Yet.
